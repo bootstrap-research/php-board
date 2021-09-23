b40dac1ad90628d2a4800f7f5726c6204cb78fd6
Alayers <langml0810@naver.com>
2021-09-23 23:43:44 +0900

    foreach

--- view/main.php
+++ view/main.php
@@ -32,83 +32,32 @@
             </div>
         </nav>
         
+        <?php 
+            require "../config/db_connect.php";
+
+            $stmt = $conn -> prepare("SELECT * FROM board");
+            $stmt -> execute();
+            $boardResult = $stmt -> fetchAll();
+        ?>
         
         <div class="container">
             <div class="row col-15">
+
+                <?php foreach($boardResult as $boardRow) { ?>
                 
-                 <div class="card mt-5 ms-3" style="width: 18rem;">
-                    <img src="" class="card-img-top" alt="">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-dark">Go somewhere</a>
-                        </div>
-                </div>
-
                 <div class="card mt-5 ms-3" style="width: 18rem;">
                     <img src="" class="card-img-top" alt="">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-dark">Go somewhere</a>
-                        </div>
-                </div>
 
-                <div class="card mt-5 ms-3" style="width: 18rem;">
-                    <img src="" class="card-img-top" alt="">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-dark">Go somewhere</a>
-                        </div>
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $boardRow['title'] ?></h5>
+                        <p class="card-text"><?php echo $boardRow['contents'] ?></p>
+                        
+                        <a href="..view/detailpage.php" class="btn btn-dark">detail Page</a>
+                    </div>
                 </div>
-
-                <div class="card mt-5 ms-3" style="width: 18rem;">
-                    <img src="" class="card-img-top" alt="">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-dark">Go somewhere</a>
-                        </div>
-                </div>
-
-                <div class="card mt-5 ms-3" style="width: 18rem;">
-                    <img src="" class="card-img-top" alt="">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-dark">Go somewhere</a>
-                        </div>
-                </div>
-
-                <div class="card mt-5 ms-3" style="width: 18rem;">
-                    <img src="" class="card-img-top" alt="">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-dark">Go somewhere</a>
-                        </div>
-                </div>
-
-                <div class="card mt-5 ms-3" style="width: 18rem;">
-                    <img src="" class="card-img-top" alt="">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-dark">Go somewhere</a>
-                        </div>
-                </div>
-
-                <div class="card mt-5 ms-3" style="width: 18rem;">
-                    <img src="" class="card-img-top" alt="">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-dark">Go somewhere</a>
-                        </div>
-                </div>
-
                 
+                <?php } ?>
+
 
 
             </div>
