d88578c0c8100732dbbc895cbd88b8e3e537055a
Alayers <langml0810@naver.com>
2021-09-27 23:39:34 +0900

    error

--- auth/loginAuth.php
+++ auth/loginAuth.php
@@ -4,6 +4,7 @@ session_start();
 require "../config/db_connect.php";     //DB 
 
 
+
 if(isset($_POST["email"]) && isset($_POST["password"])              //공복 검사
     && !empty($_POST["email"]) && !empty($_POST["password"])) {
 
@@ -18,17 +19,20 @@ if(isset($_POST["email"]) && isset($_POST["password"])              //공복 검
         $stmt -> execute(array($email));
         $user = $stmt -> fetch();
 
-        $email = $user["email"];
-        $password = $user["password"];
-        $username = $user["name"];
+        $user_email = $user["email"];
+        $user_password = $user["password"];
+        $user_username = $user["username"];
 
 
-        if ($email === $email) {            //같은지 비교 
-            if ($password === $password) {
-                $_SESSION["email"] = $email;
-                $_SESSION["username"] = $username;
+        if ($email === $user_email) {            //같은지 비교 
+            if ($password === $user_password) {
+                $_SESSION["email"] = $user_email;
+                $_SESSION["username"] = $user_username;
 
                 header("Location: ../view/main.php");
+            } else {
+                echo("<script>alert('Wrong Password');</script>");
+                header("Location: ../view/login.php");
             }
         }
         
