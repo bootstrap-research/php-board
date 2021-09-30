6aaa12123eff9d818473639dd35e2df75abb72dc
Alayers <langml0810@naver.com>
2021-09-30 10:09:26 +0900

    modify

--- auth/modifyAuth.php
+++ auth/modifyAuth.php
@@ -15,13 +15,32 @@
         if (filter_var($email, FILTER_VALIDATE_EMAIL)) {      //유효성 검사(email 구문 확인)
         echo("$email is a valid email address");
 
+        // $User_id = $_GET['User_id'];
 
-        if(filter_var($newpassword === $Confirmpassword)) {      //유효성 검사(비밀번호 와 확인 비밀번호가 동일한지 확인)
-        echo("$newpassword is a notvaild password");
-        
+        $stmt = $conn -> prepare("SELECT * FROM users WHERE email = ?"); 
+        $stmt -> execute(array($email));
+        $user = $stmt -> fetch();
+
+        print_r("$user");
+
+        $user_email = $user["email"];
+        $user_password = $user["password"];
+
+
+        if (filter_var($password === $user_password)) {
+
+            if(filter_var($newpassword === $Confirmpassword)) {      //유효성 검사(비밀번호 와 확인 비밀번호가 동일한지 확인)
+            echo("$newpassword is a vaild password");
+            
+            } else {
+            echo("$Confirmpassword is a not vaild password");
+            }
+
         } else {
-        echo("$password is a not vaild password");
+            echo("$password is a not vaild password");
         }
+
+
 
         
         $stmt = $conn -> prepare("UPDATE users SET username=:username, email=:email, password=:pwd WHERE user_id=:userId");     //SQL INSERT문 
@@ -30,7 +49,7 @@
         $_SESSION['username'] = $username;
         $_SESSION['email'] = $email;
 
-        header("Location:../view/modify.php");
+        header("Location:../view/main.php");
         
     } else {    
         echo("$email is not a valid email address");
--- view/modify.php
+++ view/modify.php
@@ -6,6 +6,32 @@
    <meta charset="utf-8">
 </head>
     <body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+            <div class="container-fluid">
+
+                <a class="navbar-brand" href="/view/main.php">PHP-BOARD</a>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+
+
+                    </ul>
+                            <form action="/view/write.php" class="d-flex" style="margin-right: 1%;">
+                            <button class="btn btn-dark" type="submit">Write</button>
+                            </form>
+                    
+                            <form action="../auth/logout.php" class="d-flex" >
+                            <button class="btn btn-dark" type="submit">Logout</button>
+                            </form>
+                </div>
+            </div>
+        </nav>
+
+
+
         <form action="../auth/modifyAuth.php" method="post">
 
             <div style="min-width: 450px" class="rounded-3 border bordr-1 shadow-lg w-25 p-5 position-absolute top-50 start-50 translate-middle">
@@ -13,41 +39,67 @@
                 <h3 class="mb-2">Edit info</h3>
 
 
-                <div class="form-floating mb-2">
-                        <input type="text" class="form-control" name="username" 
-                            id="floatingUserId" placeholder="UserId" required="required">
-                        <label for="floatingUserId">UserName</label>
-                    </div>
+                <?php 
+                require "../config/db_connect.php";
 
-                    <div class="form-floating mb-2">
-                        <input type="email" class="form-control" name="email"
-                            id="floatingInput" placeholder="name@example.com" > 
-                        <label for="floatingInput">Email address</label>
-                    </div>
+                $stmt = $conn -> prepare("SELECT * FROM users");
+                $stmt -> execute();
+                $boardResult = $stmt -> fetch();
+                ?>
 
-                    <div class="form-floating mb-2">
-                        <input type="password" class="form-control" name="password"
-                            id="floatingPassword" placeholder="Password" required="required">
-                        <label for="floatingPassword">Password</label>
-                    </div>
 
-                    <div class="form-floating mb-2">
-                        <input type="password" class="form-control" name="newpassword"
-                            id="floatingPassword" placeholder="Password" value="echo">
-                        <label for="floatingPassword"> New Password</label>
-                    </div>
+                        <div class="form-floating mb-2">
 
-                    <div class="form-floating mb-1">
-                        <input type="password" class="form-control" name="Confirmpassword"
-                            id="floatingPassword" placeholder="Password" >
-                        <label for="floatingConfirmPassword">Confirm Password</label>
-                    </div>
+                            <input type="text" class="form-control" name="username" 
+                                id="floatingUserId" placeholder="UserId" 
+                                value="<?php echo $boardResult['username'] ?>" >
 
-                    <div class="row">
-                            <button type="submit" class="btn btn-dark col ms-3" >Save</button>
-                            <a href="../view/main.php" class="btn btn-dark col ms-2 me-3" id="liveAlertBtn">Cancle</a>
-                    </div>
-            
+                            <label for="floatingUserId">UserName</label>
+                        </div>
+
+
+                        <div class="form-floating mb-2">
+
+                            <input type="email" class="form-control" name="email"
+                                id="floatingInput" placeholder="name@example.com" 
+                                value="<?php echo $boardResult['email'] ?>" > 
+
+                            <label for="floatingInput">Email address</label>
+                        </div>
+
+
+                        <div class="form-floating mb-2">
+
+                            <input type="password" class="form-control" name="password"
+                                id="floatingPassword" placeholder="Password" required="required">
+
+                            <label for="floatingPassword">Password</label>
+                        </div>
+
+
+                        <div class="form-floating mb-2">
+
+                            <input type="password" class="form-control" name="newpassword"
+                                id="floatingPassword" placeholder="Password" value="">
+
+                            <label for="floatingPassword"> New Password</label>
+                        </div>
+
+
+                        <div class="form-floating mb-1">
+
+                            <input type="password" class="form-control" name="Confirmpassword"
+                                id="floatingPassword" placeholder="Password" >
+
+                            <label for="floatingConfirmPassword">Confirm Password</label>
+                        </div>
+
+
+                        <div class="row">
+                                <button type="submit" class="btn btn-dark col ms-3" >Save</button>
+                                <a href="../view/main.php" class="btn btn-dark col ms-2 me-3" id="liveAlertBtn">Cancle</a>
+                        </div>
+
             </div>
 
         </form>
